a9e64253e8a3987a842fa1bcc3290dfb5caa136a
Ryan Schmidt <skizzerz@skizzerz.net>
2019-06-13 15:27:44 -0700

    Sigh

--- style.py
+++ style.py
@@ -1,8 +1,10 @@
+from pygments.style import Style
 from pygments.styles.default import DefaultStyle
 from pygments.token import Error
 
 
-class SimpleStyle(DefaultStyle):
-    def __init__(self):
-        # don't put a red box around stuff
-        del self.styles[Error]
+class SimpleStyle(Style):
+    styles = DefaultStyle.styles
+
+
+del SimpleStyle.styles[Error]
